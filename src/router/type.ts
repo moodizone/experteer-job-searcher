1a14e92cdec76a0bfd6a7c2cb0e7bbea52283203
moodi <moodizone@gmail.com>
2022-04-15 17:12:33 +0430

    ⚡️ router improvement

--- src/router/type.ts
+++ src/router/type.ts
@@ -1,8 +1,16 @@
 import React from "react";
 
 export type RouteName = "search" | "details";
+export interface PageProps {
+  /**
+   * tab title
+   * @default 'Experteer'
+   */
+  title?: string;
+}
 
 export interface RouteType {
   path: string;
+  title?: string;
   Component: React.ComponentType;
 }
