322bc57cd7e924fa250a2c8ef95bd5c8e596ba0c
moodi <moodizone@gmail.com>
2022-04-25 20:20:22 +0430

    ✨ add `useInterval` hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function useInterval(
+  callback: () => Promise<any>,
+  delay: number | null
+) {
+  const savedCallback = React.useRef<any>();
+
+  // Remember the latest callback.
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  React.useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
